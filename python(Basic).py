23de34938a76b9b51521b11ca41ad6569cd8503a
Anshika <sarohaanshika19@gmail.com>
2026-03-26 13:06:32 +0530

    Updated

--- python(Basic).py
+++ python(Basic).py
@@ -1,1 +1,54 @@
-print("Hello World")
+#1. Write a program to print "Hello Python" on the screen. 
+print("Hello Python")
+
+#2. Write a program to take your name as input and print "Hello <name>".
+name = input("Enter the name : ")
+print("Hello" , name)
+
+#3. Write a program to take one integer input from the user and print it. 
+a = int(input("Enter an interger : "))
+print("Entered Integer value :", a)
+
+
+#4. Write a program to take two integers as input and print their sum. 
+a = int(input("Enter 1st value :"))
+b = int(input("Enter 2nd value :"))
+print("You Entered : ", a + b)
+
+#5. Write a program to take two integers as input and print their multiplication. 
+a = int(input("Enter 1st value :"))
+b = int(input("Enter 2nd value :"))
+print("You Entered : ", a * b)
+
+#6. Write a program to take one float number as input and print it. 
+num = float(input("Enter the number : "))
+print("You entered : ", num)
+
+#7. Write a program to take two float numbers as input and print their average.
+#hint :- To calculate the average of two float numbers : average = (a+b)/2
+
+a = float(input("Enter 1st value : "))
+b = float(input("Enter 2nd value : "))
+average = (a+b)/2
+print("Average : ", average)
+
+
+#8. Write a program to take length and width (float) from the user and calculate the area of 
+a rectangle.
+l = float(input("Enter the length of the rectangle : "))
+w = float(input("Enter the width of the rectangle : "))
+area = l*w
+print("Area of the rectangle :", area)
+
+
+#9. Write a program to take an integer input and convert it into float, then print it. 
+num = int(input("Enter an interger : "))
+num = float(num)
+print("Float num :", num)
+
+#10. Write a program to take price of three items (float) and print the total price.
+A = float(input("Enter the price of A item : "))
+B = float(input("Enter the price of B item : "))
+C = float(input("Enter the price of C item : "))
+total_price = A+B+C
+print("Total Price :", total_price)
---
+++ while_loop_exercise.py
@@ -0,0 +1,58 @@
+#Part 1 – Basic Level
+
+#1. Print numbers from 1 to 10 using a while loop.
+
+i = 1
+while i <= 10:
+    print(i)
+    i = i + 1
+
+    
+#2. Print even numbers from 1 to 20.
+
+i = 2
+while i <= 20:
+    print(i)
+    i = i + 2
+
+#3. Print odd numbers from 1 to 20.
+
+i = 1
+while i <= 20:
+    print(i)
+    i = i + 2
+    
+#4. Print numbers from 10 to 1 (reverse order).
+
+i = 10
+while i >= 1:
+    print(i)
+    i = i - 1
+    
+#5. Print multiplication table of 5 using while loop.
+
+i = 5
+while i <= 50:
+    print(i)
+    i = i + 5
+
+
+#Part 2 – Intermediate Level
+
+#6. Find the sum of first 10 natural numbers using while loop.
+
+i = 1
+sum = 0
+
+while i <= 10:
+    sum = sum + i
+    i = i + 1
+
+print("Sum =", sum)
+
+
+#7. Find factorial of a number entered by user.
+
+
+
+    
